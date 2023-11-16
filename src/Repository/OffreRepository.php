b7981b4b6764b8c2c75648453dd2d7746c611aab
Baptiste Simon <baptiste.simon@etudiant.univ-reims.fr>
2023-11-16 10:17:22 +0100

    ajout de l'entiter Offre et du Repository OffreRepository

---
+++ src/Repository/OffreRepository.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\Offre;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @extends ServiceEntityRepository<Offre>
+ *
+ * @method Offre|null find($id, $lockMode = null, $lockVersion = null)
+ * @method Offre|null findOneBy(array $criteria, array $orderBy = null)
+ * @method Offre[]    findAll()
+ * @method Offre[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class OffreRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, Offre::class);
+    }
+
+//    /**
+//     * @return Offre[] Returns an array of Offre objects
+//     */
+//    public function findByExampleField($value): array
+//    {
+//        return $this->createQueryBuilder('o')
+//            ->andWhere('o.exampleField = :val')
+//            ->setParameter('val', $value)
+//            ->orderBy('o.id', 'ASC')
+//            ->setMaxResults(10)
+//            ->getQuery()
+//            ->getResult()
+//        ;
+//    }
+
+//    public function findOneBySomeField($value): ?Offre
+//    {
+//        return $this->createQueryBuilder('o')
+//            ->andWhere('o.exampleField = :val')
+//            ->setParameter('val', $value)
+//            ->getQuery()
+//            ->getOneOrNullResult()
+//        ;
+//    }
+
+    public function findByRecent():array{
+        $qb = $this->createQueryBuilder('o')
+            ->select('o')
+            ->orderBy('o.jourDeb', 'DESC') // Assuming you want to order by the 'jourDeb' field
+            ->setMaxResults(10);
+
+        return $qb->getQuery()->getResult();
+    }
+}
+
