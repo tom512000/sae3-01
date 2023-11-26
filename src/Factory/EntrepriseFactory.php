bec3c7814ddf655bf16c955d3707569392810442
Tom Sikora <tom.sikora@etudiant.univ-reims.fr>
2023-11-26 18:30:17 +0100

    Finitions des offres (offre/index.html.twig et css/style.css)

--- src/Factory/EntrepriseFactory.php
+++ src/Factory/EntrepriseFactory.php
@@ -95,8 +95,8 @@ final class EntrepriseFactory extends ModelFactory
 
     protected function generateLogo($Name): string
     {
-        $color1 = self::faker()->safeColorName();
-        $color2 = self::faker()->safeColorName();
+        $color1 = substr(self::faker()->safeHexColor(), 1);
+        $color2 = substr(self::faker()->safeHexColor(), 1);
         return "https://placehold.co/500x500/{$color1}/{$color2}?text={$Name}";
     }
 
