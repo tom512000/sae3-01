2def4392e4a4e07cb2000063cb7850ef41c720a1
Simon <baptiste.simon@etudiant.univ-reims.fr>
2023-12-20 18:16:24 +0100

    ajout des fichiers de test et debut des tests

---
+++ tests/Controller/Profil/IndexCest.php
@@ -0,0 +1,18 @@
+<?php
+
+
+namespace App\Tests\Controller\Profil;
+
+use App\Tests\Support\ControllerTester;
+
+class IndexCest
+{
+    public function _before(ControllerTester $I)
+    {
+    }
+
+    // tests
+    public function tryToTest(ControllerTester $I)
+    {
+    }
+}
